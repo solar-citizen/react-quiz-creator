7263da59b064685e100e6a2a74dd5b74ea1731c3
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-01-20 05:38:10 +0200

    MenuToggle

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,12 +1,31 @@
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Quiz from '../../Containers/Quiz/Quiz';
+import MenuToggle from '../../Components/Navigation/MenuToggle/MenuToggle';
 
-const Layout = () => {
-  return (
-    <div className={classes.Layout}>
-      <Quiz />
-    </div>
-  );
-};
+class Layout extends Component {
+  state = {
+    menu: false,
+  };
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
+
+        <Quiz />
+      </div>
+    );
+  }
+}
 
 export default Layout;
