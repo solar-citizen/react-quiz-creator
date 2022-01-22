d140f64ad1a4db6608500c73374e3121ff9a5de2
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-01-22 07:56:26 +0200

    Authorization page

--- src/Containers/Auth/Auth.jsx
+++ src/Containers/Auth/Auth.jsx
@@ -1,7 +1,32 @@
+import classes from './Auth.module.css';
+import Button from '../../Components/UI/Button/Button';
+
 const Auth = () => {
+  const loginHandler = () => {};
+
+  const registerHandler = () => {};
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Auth</h1>
+    <div className={classes.Auth}>
+      <div>
+        <h1>Authorization</h1>
+
+        <form className={classes.AuthForm} onSubmit={submitHandler}>
+          <input type='text' />
+          <input type='text' />
+
+          <Button type='success' onClick={loginHandler}>
+            Log in
+          </Button>
+          <Button type='primary' onClick={registerHandler}>
+            Register
+          </Button>
+        </form>
+      </div>
     </div>
   );
 };
