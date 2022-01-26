64d4f167d844712e157d8511f74deb74d9cfd577
Vladyslav Tishchenko <fsknnn@gmail.com>
2022-01-26 05:09:43 +0200

    QuizList backend

--- src/Containers/QuizList/QuizList.jsx
+++ src/Containers/QuizList/QuizList.jsx
@@ -4,24 +4,39 @@ import classes from './QuizList.module.css';
 import axios from 'axios';
 
 export default class QuizList extends Component {
+  state = {
+    quizes: [],
+  };
+
   renderQuizes = () => {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li key={index}>
-          <NavLink to={'/quiz/' + quiz}>Test {quiz}</NavLink>
+        <li key={quiz.id}>
+          <NavLink to={'/quiz/' + quiz.id}>{quiz.name}</NavLink>
         </li>
       );
     });
   };
 
-  componentDidMount() {
-    axios
-      .get(
-        'https://react-quiz-2-929a0-default-rtdb.europe-west1.firebasedatabase.app/quiz.json'
-      )
-      .then((response) => {
-        console.log(response);
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        'https://react-quiz-2-929a0-default-rtdb.europe-west1.firebasedatabase.app/quizes.json'
+      );
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Quiz #${index + 1}`,
+        });
       });
+
+      this.setState({ quizes });
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
